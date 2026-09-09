734b779f07441626dc33809ac30652b176f62943
Y1001-y <1744272885@qq.com>
2026-09-09 10:36:55 +0800

    chore: 创建了后端工程化学习仓库

---
+++ src/test/java/top/mqxu/springboot/BackendLearningApplicationTests.java
@@ -0,0 +1,13 @@
+package top.mqxu.springboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendLearningApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
